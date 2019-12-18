1e023ac2a99cbe52197b552f44c6c8c11d968f31
xxg <qq1102746402@163.com>
2019-12-18 21:37:46 +0800

    完成布局和样式 并完成部分自动验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="zhu">
     <el-card class="box-card">
-
+        <div class="top">
+            <img src="../../assets/img/logo_index.png" alt="">
+        </div>
+      <el-form ref="form" :model="form" :rules="rules">
+        <el-form-item prop="moble">
+          <el-input v-model="form.moble" placeholder='输入手机号码'></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="form.code" style="width:65%" placeholder='输入验证码'></el-input>
+          <el-button plain style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop='check'>
+          <!-- `checked` 为 true 或 false -->
+          <el-checkbox v-model="form.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+             <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      form: { moble: '', code: '', check: false },
+      rules: {
+        moble: [{ required: true, message: '请输入手机号码' }],
+        code: [{ required: true, message: '请输入验证码' }],
+        check: []
+      }
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
@@ -19,9 +48,15 @@ export default {}
   justify-content: center;
   align-items: center;
   .box-card {
-    width: 600px;
+    width: 576px;
+  }
+  .top{
+      text-align: center
 
   }
+  .top  img{
+          height: 45px;
+          vertical-align: baseline
+      }
 }
-
 </style>
