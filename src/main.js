8bb739c59df5fd7efff7241798ebabde9cf7e0f8
xxg <qq1102746402@163.com>
2019-12-18 20:14:24 +0800

    安装引入element

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import element from 'element-ui'
 
 Vue.config.productionTip = false
-
+Vue.use(element)
 new Vue({
   router,
   render: h => h(App)
